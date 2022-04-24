3c0a13d198bfbd89ad7a37029e2304bbf250a67e
Wladislaw <65966048+Innaviton@users.noreply.github.com>
2022-04-24 13:11:24 +0300

    Конец

--- all/src/main.js
+++ all/src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
@@ -12,3 +13,19 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+=======
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import VueCountdownTimer from 'vuejs-countdown-timer';
+Vue.use(VueCountdownTimer);
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+>>>>>>> Stashed changes
